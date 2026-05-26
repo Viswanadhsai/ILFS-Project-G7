a621968182041b866d2d3bca9d6551a81e8d24c5
shaiktoufeeq07 <umarshaik6462@gmail.com>
2026-05-26 21:01:53 +1000

    addede auth middleware to protect matching routes

--- backend/routes/matching.routes.js
+++ backend/routes/matching.routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { matchItems } = require("../controllers/matching.controller");
+const { protect } = require("../middleware/auth.middleware");
 
-router.get("/", matchItems);
+router.get("/", protect, matchItems);
 
 module.exports = router;
