14c8ebf80a5eabb1844fa82a92d6346515420e76
shaiktoufeeq07 <umarshaik6462@gmail.com>
2026-05-27 18:36:06 +1000

    added get all claims route to admin routes

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllItems, getAllMatches } = require("../controllers/admin.controller");
+const { getAllItems, getAllMatches, getAllClaims } = require("../controllers/admin.controller");
 const { protect } = require("../middleware/auth.middleware");
 
 router.get("/items", protect, getAllItems);
 router.get("/matches", protect, getAllMatches);
+router.get("/claims", protect, getAllClaims);
 
 module.exports = router;
